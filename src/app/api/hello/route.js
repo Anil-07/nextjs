237ae29d540fd69fe33d18a48c67d22b8adf5737
Anil Kumar <ak48021@gmail.com>
2023-10-03 03:06:34 +0530

    test v2 env

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -3,5 +3,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello from the API route!" });
+  return NextResponse.json({
+    message: `Hello from the API route! ${process.env.TEST_ENV}`,
+  });
 }
